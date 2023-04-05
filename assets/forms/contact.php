49bca71837a44bce65403e3db7b326ddbd27fd62
neutroon <191900312@ecu.edu.eg>
2023-04-06 01:57:06 +0200

    #

--- assets/forms/contact.php
+++ assets/forms/contact.php
@@ -57,5 +57,5 @@
     file_get_contents(url . "sendmessage?chat_id=" . $chat_id . "&text=" . $message . "&parse_mode=HTML");
 
 }
-header('location: ../../index.html');
+header("Location: http://".$_SERVER['HTTP_HOST']);
 ?>
