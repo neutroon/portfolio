11f94d4b81a2dff24708d85629a39b9dbbee4815
neutroon <191900312@ecu.edu.eg>
2024-01-29 00:56:11 +0200

    #

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {path: "", redirectTo: "projects", pathMatch: 'full'},
   {path: "projects", loadComponent: ()=> import('./components/projects/projects.component').then( (x)=> x.ProjectsComponent )},
   {path: "skills", loadComponent: () => import('./components/skills/skills.component').then( (x) => x.SkillsComponent )},
   {path: "experience", loadComponent: ()=> import('./components/experience/experience.component').then( (x) => x.ExperienceComponent ) }
