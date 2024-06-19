6467443acb94f3d0d50281dc777e360f9d7c59da
neutroon <191900312@ecu.edu.eg>
2024-06-19 08:50:12 +0300

    git some github info and add spinner when loading for data from github

--- src/app/components/profile-nav/profile-nav.component.ts
+++ src/app/components/profile-nav/profile-nav.component.ts
@@ -6,6 +6,12 @@ import { PrimeNGConfig } from 'primeng/api';
 import { TranslateModule } from '@ngx-translate/core';
 import { GetDataService } from '../../shared/services/get-data.service';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { GithubGraphqlService } from '../../shared/services/github-graphql.service';
+import { Apollo, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
+import { HttpHeaders } from '@angular/common/http'; // Import HttpHeaders
+import { ProgressSpinnerComponent } from '../progress-spinner/progress-spinner.component';
 @Component({
   selector: 'profile-nav',
   standalone: true,
@@ -15,6 +21,27 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
     RouterModule,
     SettingComponent,
     TranslateModule, // Add TranslateModule here
+    ProgressSpinnerComponent,
+  ],
+  providers: [
+    GithubGraphqlService,
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://api.github.com/graphql',
+            headers: new HttpHeaders({
+              // Use HttpHeaders object
+              Authorization: `Bearer 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`,
+            }),
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
+    Apollo,
   ],
   templateUrl: './profile-nav.component.html',
   styleUrl: './profile-nav.component.scss',
@@ -23,7 +50,8 @@ export class ProfileNavComponent implements OnInit {
   constructor(
     private _PrimeNGConfig: PrimeNGConfig,
     private _Renderer2: Renderer2,
-    private _GetDataService: GetDataService
+    private _GetDataService: GetDataService,
+    private _GithubGraphqlService: GithubGraphqlService
   ) {}
   isSettingActive: boolean = false;
 
@@ -33,7 +61,9 @@ export class ProfileNavComponent implements OnInit {
 
   links!: any[];
   projects!: any[];
-
+  totalContributions!: number;
+  reposCount!: number;
+  totalCommitsCount!: number;
   ngOnInit(): void {
     this._PrimeNGConfig.ripple = true;
 
@@ -47,6 +77,32 @@ export class ProfileNavComponent implements OnInit {
     this._GetDataService.getLinks().subscribe({
       next: (res) => {
         this.links = res;
+      },
+    });
+
+    // get contributions
+    const username = 'neutroon'; // Replace with the GitHub username you want to query
+    this._GithubGraphqlService.getTotalContributions(username).subscribe({
+      next: (res) => {
+        this.totalContributions = res;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+    // get repos count
+    this._GithubGraphqlService.getUserRepoCount(username).subscribe({
+      next: (res) => {
+        this.reposCount = res;
+      },
+    });
+    // get latest commits count
+    this._GithubGraphqlService.getTotalLatestCommitsCount(username).subscribe({
+      next: (res) => {
+        this.totalCommitsCount = res;
+      },
+      error: (err) => {
+        console.error(err);
       },
     });
   }
