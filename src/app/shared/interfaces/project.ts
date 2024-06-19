1082835243f47d8293946fd79e2947946ba2c0ff
neutroon <191900312@ecu.edu.eg>
2024-06-19 08:14:57 +0300

    add project interface

---
+++ src/app/shared/interfaces/project.ts
@@ -0,0 +1,14 @@
+export interface project {
+  mobile: {
+    device: string;
+    gif: string;
+    url: string;
+  };
+  desktop: {
+    device: string;
+    gif: string;
+    url: string;
+  };
+  ischangeViewLoading: boolean;
+  id: string;
+}
